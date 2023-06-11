47ba7d1f96e9b172292c9834b89ae7bca1389615
LostRoses <joao.00000829607@unicap.br>
2023-06-11 15:42:22 -0300

    alterações finais

--- P3/view/main.java
+++ P3/view/main.java
@@ -2,7 +2,7 @@ package P3.view;
 
 import P3.code.controller.*;
 
-class main {
+class Main {
 
     public static void main(String[] args) throws IllegalAccessException {
         Menus.menuPrincipal();
